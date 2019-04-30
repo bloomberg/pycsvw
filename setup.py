aea24decb15d5bda3a5bd25c105c8af3cfe597ff
fromjss <fromjss@users.noreply.github.com>
2019-04-30 09:26:14 -0400

    Source version from __version__, add PyTestRunner

--- setup.py
+++ setup.py
@@ -9,15 +9,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import shlex
 from setuptools import setup, find_packages
+from pycsvw import __version__
+
+try:
+    from setuptools.command.test import test as UnitTester
+    
+    class PyTestRunner(UnitTester):
+        """ 
+        Ref: https://docs.pytest.org/en/latest/goodpractices.html#manual-integration
+        """
+        user_options = [("pytest-args=", "a", "Arguments to pass to pytest")]
+        def initialize_options(self):
+            UnitTester.initialize_options(self)
+            self.pytest_args = ''
+        def run_tests(self):
+            """ imports in here, cause outside the eggs aren't loaded"""
+            import pytest
+            rc = pytest.main(shlex.split(self.pytest_args))
+            raise SystemExit(rc)
+except:
+    pass
 
 setup(
     name='pycsvw',
-    version="1.0.2",
+    version=__version__,
     description='Generate JSON and RDF from csv files with metadata',
     url='https://github.com/bloomberg/pycsvw',
     author='Dev Ramudit, Erman Korkut',
     author_email='dramudit2@bloomberg.net, ekorkut1@bloomberg.net',
+
+    license='MIT',
 
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -31,6 +54,8 @@ setup(
     packages=find_packages(),
     install_requires=['click', 'six', 'future', 'rdflib', 'rdflib-jsonld', 'python-dateutil'],
     tests_require=['pytest', 'mock'],
+    test_suite='tests',
+    cmdclass={'pytest': PyTestRunner},
     entry_points='''
         [console_scripts]
         pycsvw=pycsvw.scripts.cli:main        
